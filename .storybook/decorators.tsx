af4547102b493b810fb38600b6f44fff73027108
Sergei Maertens <sergei@maykinmedia.nl>
2023-04-07 15:53:10 +0200

    :hammer: Don't let dialogs do the actual overlay in storybook

--- .storybook/decorators.tsx
+++ .storybook/decorators.tsx
@@ -3,8 +3,11 @@ import React from 'react';
 export const ModalDecorator = (Story, {parameters}) => {
   if (parameters?.modal?.noModal) return <Story />;
   return (
-    <div className="formio-dialog formio-dialog-theme-default component-settings">
-      <div className="formio-dialog-overlay"></div>
+    <div
+      className="formio-dialog formio-dialog-theme-default component-settings"
+      style={{position: 'relative'}}
+    >
+      <div className="formio-dialog-overlay" style={{position: 'relative'}}></div>
       <div className="formio-dialog-content">
         <div></div>
         <button
