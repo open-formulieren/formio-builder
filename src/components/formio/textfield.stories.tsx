681f185a42789e2cc7b8694bdbf4c97fd3660e46
Sergei Maertens <sergei@maykinmedia.nl>
2023-05-09 20:16:45 +0200

    :sparkles: [#7] -- support rendering a textfield in multiple mode
    * Added support for rendering without label/description when only a
      bare input is requested
    * Added interaction tests for the multiple behaviour
    * Export the multi-enable wrapped component by default

--- src/components/formio/textfield.stories.tsx
+++ src/components/formio/textfield.stories.tsx
@@ -1,7 +1,7 @@
 import withFormik from '@bbbtech/storybook-formik';
 import {expect} from '@storybook/jest';
 import {ComponentMeta, ComponentStory} from '@storybook/react';
-import {within} from '@storybook/testing-library';
+import {userEvent, within} from '@storybook/testing-library';
 
 import TextField from './textfield';
 
@@ -37,6 +37,45 @@ WithToolTip.args = {
   label: 'With tooltip',
   tooltip: 'Hiya!',
   required: false,
+};
+
+export const Multiple = Template.bind({});
+Multiple.args = {
+  label: 'Multiple inputs',
+  description: 'Array of strings instead of a single string value',
+  multiple: true,
+};
+Multiple.parameters = {
+  formik: {
+    initialValues: {'my-textfield': ['first value']},
+  },
+};
+Multiple.play = async ({canvasElement}) => {
+  const canvas = within(canvasElement);
+
+  // check that new items can be added
+  await userEvent.click(canvas.getByRole('button', {name: 'Add another'}));
+  const input1 = await canvas.getByTestId('input-my-textfield[0]');
+  await expect(input1).toHaveDisplayValue('first value');
+  await userEvent.clear(input1);
+  await userEvent.type(input1, 'Foo');
+  await expect(input1).toHaveDisplayValue('Foo');
+
+  const input2 = await canvas.getByTestId('input-my-textfield[1]');
+  await expect(input2).toHaveDisplayValue('');
+
+  // the label & description should be rendered only once, even with > 1 inputs
+  await expect(canvas.queryAllByText('Multiple inputs')).toHaveLength(1);
+  await expect(
+    canvas.queryAllByText('Array of strings instead of a single string value')
+  ).toHaveLength(1);
+
+  // finally, it should be possible delete rows again
+  const removeButtons = await canvas.findAllByRole('button', {name: 'Remove item'});
+  await expect(removeButtons).toHaveLength(2);
+  await userEvent.click(removeButtons[0]);
+  await expect(canvas.getByTestId('input-my-textfield[0]')).toHaveDisplayValue('');
+  await expect(canvas.queryByTestId('input-my-textfield[1]')).not.toBeInTheDocument();
 };
 
 export const WithErrors = Template.bind({});
