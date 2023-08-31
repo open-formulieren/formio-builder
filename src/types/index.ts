c3e8aa9f642493d02ba526c465e63f8e26fff546
Sergei Maertens <sergei@maykinmedia.nl>
2023-08-31 15:33:27 +0200

    :arrow_up: [#31] Bump to more precise type definitions for date constraint validation

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,3 +7,22 @@ export function hasOwnProperty<X extends {}, Y extends PropertyKey>(
 ): obj is X & Record<Y, unknown> {
   return obj.hasOwnProperty(prop);
 }
+
+/**
+ * Filter a given type by a given expected value type.
+ *
+ * E.g. with a type:
+ *
+ * interface Test {
+ *   foo: string;
+ *   bar: string;
+ *   baz: number;
+ * };
+ *
+ * FilterByValueType<Test, string> == {foo: string, bar: string}
+ *
+ * See https://stackoverflow.com/a/63990350
+ */
+export type FilterByValueType<T, VT> = {
+  [P in keyof T as T[P] extends VT ? P : never]: T[P];
+};
