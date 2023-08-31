689d2c3dfa745653cdcbdab8fa452acca1db6c75
Sergei Maertens <sergei@maykinmedia.nl>
2023-08-31 17:45:46 +0200

    :sparkles: [#31] Initialize field values for constraint validator
    When the mode of a min/max date constraint validation configuration
    changes, other fields become relevant. E.g. 'future' or 'past' can
    specify if today should be included or not, while 'fixedValue' does
    not require any such extra input.
    
    What exactly must be configured for each mode is declared in a type-safe
    way to ensure it's complete and as concise as possible.
    
    The select dropdown now accepts an optional onChange callback so you
    can run additional callbacks on change events, which is particularly
    interesting with React's batching of state updates and reduces the
    amount of re-renders and/or ambiguous transitive states.

--- src/registry/date/validation.tsx
+++ src/registry/date/validation.tsx
@@ -12,10 +12,13 @@ import {
 import {Panel, Select} from '@/components/formio';
 import {FilterByValueType} from '@/types';
 
+// A bunch of derived types from the DateComponentSchema that makes working with the
+// schema a bit more readable while keeping everything exhaustive and type safe.
 type AllDateExtensions = Required<NonNullable<DateComponentSchema['openForms']>>;
 type AllModes = DateConstraintConfiguration['mode'];
 type NonEmptyModes = Exclude<AllModes, ''>;
 type DateConstraintKey = keyof FilterByValueType<AllDateExtensions, {mode: AllModes}>;
+type AllPossibleConstraints = AllDateExtensions[DateConstraintKey];
 
 // Mappings of value-label to produce dropdown options. Note that you need to filter out
 // only the options relevant to the particular field.
@@ -62,8 +65,28 @@ const MODES_TO_EXCLUDE: Record<DateConstraintKey, NonEmptyModes[]> = {
   maxDate: ['future'],
 };
 
+const DEFAULT_VALUES: {
+  [K in AllPossibleConstraints['mode']]: Omit<Extract<AllPossibleConstraints, {mode: K}>, 'mode'>;
+} = {
+  '': {},
+  fixedValue: {},
+  future: {includeToday: false},
+  past: {includeToday: false},
+  relativeToVariable: {
+    variable: '',
+    delta: {
+      years: null,
+      months: null,
+      days: null,
+    },
+    operator: 'add',
+  },
+};
+
 const ModeSelect: React.FC<DateConstraintProps> = ({constraint}) => {
+  const fieldName = `openForms.${constraint}.mode`;
   const intl = useIntl();
+  const {setFieldValue} = useFormikContext<DateComponentSchema>();
 
   // filter out the validation modes not relevant for this particular constraint
   const modesToExclude = MODES_TO_EXCLUDE[constraint];
@@ -73,7 +96,7 @@ const ModeSelect: React.FC<DateConstraintProps> = ({constraint}) => {
 
   return (
     <Select
-      name={`openForms.${constraint}.mode`}
+      name={fieldName}
       label={
         <FormattedMessage
           description="Label for 'date constraint mode' builder field"
@@ -84,6 +107,18 @@ const ModeSelect: React.FC<DateConstraintProps> = ({constraint}) => {
         ...item,
         label: intl.formatMessage(item.label),
       }))}
+      isClearable
+      onChange={event => {
+        // the select *always* sets the selected value in the state, we can just use an
+        // additional onChange here (as better alternative to useEffect).
+        // The idea here is that we set the default, empty values that are mode-specific
+        // so that we don't get "uncontrolled-component-becomes-controlled" warning
+        // from React. It also helps in clearing out any left-over state from other
+        // modes when switching between them.
+        const mode: AllModes = event.target.value ?? '';
+        const emptyDefaults = {mode, ...DEFAULT_VALUES[mode]};
+        setFieldValue(`openForms.${constraint}`, emptyDefaults);
+      }}
     />
   );
 };
@@ -103,6 +138,9 @@ const DateConstraintValidation: React.FC<DateConstraintProps> = ({constraint}) =
   const intl = useIntl();
   const {values} = useFormikContext<DateComponentSchema>();
   const hasMode = !!values?.openForms?.[constraint]?.mode;
+
+  console.log(values?.openForms?.[constraint]);
+
   return (
     <Panel
       title={intl.formatMessage(PANEL_TITLES[constraint], {
