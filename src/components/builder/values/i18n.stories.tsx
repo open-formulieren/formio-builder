517751faf39b3d5dc6500fec5e3d3044ecac7818
Sergei Maertens <sergei@maykinmedia.nl>
2023-11-13 14:38:21 +0100

    :white_check_mark: Add tests for translations table

--- src/components/builder/values/i18n.stories.tsx
+++ src/components/builder/values/i18n.stories.tsx
@@ -1,5 +1,7 @@
 import {Option} from '@open-formulieren/types/lib/formio/common';
+import {expect} from '@storybook/jest';
 import {Meta, StoryObj} from '@storybook/react';
+import {within} from '@storybook/testing-library';
 
 import {withFormik} from '@/../.storybook/decorators';
 import {Component} from '@/components/formio';
@@ -71,4 +73,29 @@ export default {
 
 type Story = StoryObj<typeof ValuesTranslationsComponent>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  play: async ({canvasElement, step}) => {
+    const canvas = within(canvasElement);
+
+    await step('Check that translations are displayed', async () => {
+      const translation1 = canvas.getByLabelText('Translation for option with value "mediahaven"');
+      expect(translation1).toHaveDisplayValue('Mediahaven (NL)');
+      const translation2 = canvas.getByLabelText(
+        'Translation for option with value "keizersgracht"'
+      );
+      expect(translation2).toHaveDisplayValue('');
+    });
+
+    await step('Check order of options matches order of definition', async () => {
+      const allRows = canvas.getAllByRole('row');
+      const firstLabel = canvas.getByRole('cell', {name: 'Mediahaven'});
+      const secondLabel = canvas.getByRole('cell', {name: 'Keizersgracht'});
+      expect(firstLabel).toBeVisible();
+      expect(secondLabel).toBeVisible();
+
+      const firstOptionRow = firstLabel.closest('tr') as HTMLTableRowElement;
+      const secondOptionRow = secondLabel.closest('tr') as HTMLTableRowElement;
+      expect(allRows.indexOf(firstOptionRow)).toBeLessThan(allRows.indexOf(secondOptionRow));
+    });
+  },
+};
