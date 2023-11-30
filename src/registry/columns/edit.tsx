b89b80726858b23dff0a5f2992ab5ed0d10adb2f
Sergei Maertens <sergei@maykinmedia.nl>
2023-11-30 17:41:54 +0100

    :sparkles: [#72] Initial column configuration component

--- src/registry/columns/edit.tsx
+++ src/registry/columns/edit.tsx
@@ -1,9 +1,10 @@
-import {ColumnsComponentSchema} from '@open-formulieren/types';
-import {useFormikContext} from 'formik';
+import {Column, ColumnsComponentSchema} from '@open-formulieren/types';
+import {FieldArray, FieldArrayRenderProps, useFormikContext} from 'formik';
 import {useContext, useRef} from 'react';
 import {FormattedMessage, useIntl} from 'react-intl';
 
 import {ClearOnHide, Hidden, Key} from '@/components/builder';
+import {Component, NumberField} from '@/components/formio';
 import {BuilderContext} from '@/context';
 
 import {EditFormDefinition} from '../types';
@@ -46,8 +47,140 @@ EditForm.defaultValues = {
   ],
 };
 
+interface ColumnProps {
+  index: number;
+  arrayHelpers: FieldArrayRenderProps;
+}
+
+const ColumnRow: React.FC<ColumnProps> = ({index, arrayHelpers}) => {
+  const intl = useIntl();
+  const {getFieldProps} = useFormikContext();
+  const numCols = getFieldProps<Column[]>('columns').value?.length || 0;
+  return (
+    <>
+      <td>
+        <NumberField
+          name={`columns.${index}.size`}
+          aria-label={intl.formatMessage({
+            description: 'Accessible label for column size',
+            defaultMessage: 'Column size, value between 1 and 12.',
+          })}
+        />
+      </td>
+      <td>
+        <NumberField
+          name={`columns.${index}.sizeMobile`}
+          aria-label={intl.formatMessage({
+            description: 'Accessible label for column mobile size',
+            defaultMessage: 'Column size on mobile, value between 1 and 4.',
+          })}
+        />
+      </td>
+      <td>
+        <button
+          type="button"
+          className="btn btn-secondary formio-button-remove-row"
+          // TODO: in new design -> implement this in a proper accessible way
+          disabled={numCols <= 1}
+          aria-label={intl.formatMessage({
+            description: 'Columns table: accessible label to remove a column',
+            defaultMessage: 'Remove',
+          })}
+          onClick={() => arrayHelpers.remove(index)}
+        >
+          <i className="fa fa-times-circle-o" />
+        </button>
+      </td>
+    </>
+  );
+};
+
 const Columns: React.FC = () => {
-  return <>COLUMNS CONFIG</>;
+  const intl = useIntl();
+  const {getFieldProps} = useFormikContext();
+  const {value: columns = []} = getFieldProps<Column[] | undefined>('columns');
+
+  const tooltip = intl.formatMessage({
+    description: "Tooltip for 'columns' builder field",
+    defaultMessage: 'Specify the size of each column. The sum of all the widths should be 100%.',
+  });
+  return (
+    <Component
+      type="datagrid"
+      label={
+        <FormattedMessage
+          description="Label for the 'columns' builder field"
+          defaultMessage="Column sizes"
+        />
+      }
+      tooltip={tooltip}
+    >
+      <FieldArray name="columns">
+        {arrayHelpers => (
+          <table className="table table-bordered">
+            <thead>
+              <tr>
+                <th className="field-required">
+                  <FormattedMessage
+                    description="Column size table header/label"
+                    defaultMessage="Size"
+                  />
+                </th>
+
+                <th className="field-required">
+                  <FormattedMessage
+                    description="Column mobile size table header/label"
+                    defaultMessage="Size (mobile)"
+                  />
+                </th>
+
+                <th>
+                  <span className="sr-only">
+                    <FormattedMessage
+                      description="Column add/remove table header/label"
+                      defaultMessage="Add/remove"
+                    />
+                  </span>
+                </th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {columns.map((_, index) => (
+                <tr key={index}>
+                  <ColumnRow index={index} arrayHelpers={arrayHelpers} />
+                </tr>
+              ))}
+            </tbody>
+
+            <tfoot>
+              <tr>
+                <td colSpan={3}>
+                  <button
+                    type="button"
+                    className="btn btn-primary formio-button-add-row"
+                    onClick={() =>
+                      arrayHelpers.push({
+                        size: 6,
+                        sizeMobile: 4,
+                        components: [],
+                      } satisfies Column)
+                    }
+                  >
+                    <i className="fa fa-plus" aria-hidden="true" />
+                    <FormattedMessage
+                      description="Add another column button label"
+                      defaultMessage="Add column"
+                    />
+                  </button>
+                </td>
+              </tr>
+            </tfoot>
+          </table>
+        )}
+      </FieldArray>
+    </Component>
+  );
 };
 
 export default EditForm;
