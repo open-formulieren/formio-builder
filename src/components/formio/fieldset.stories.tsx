be1911dea2c9b772e54ef024575708981a76cb25
Viicos <65306057+Viicos@users.noreply.github.com>
2023-12-01 11:49:10 +0100

    [#65] Additionnal feedback

--- src/components/formio/fieldset.stories.tsx
+++ src/components/formio/fieldset.stories.tsx
@@ -1,5 +1,5 @@
 import {expect} from '@storybook/jest';
-import {Meta, StoryFn, StoryObj} from '@storybook/react';
+import {Meta, StoryObj} from '@storybook/react';
 import {within} from '@storybook/testing-library';
 
 import {withFormik} from '@/sb-decorators';
@@ -12,58 +12,46 @@ export default {
   decorators: [withFormik],
   parameters: {
     modal: {noModal: true},
-    // https://github.com/bbbtech/storybook-formik/issues/51#issuecomment-1136668271
-    docs: {
-      inlineStories: false,
-      iframeHeight: 150,
-    },
   },
   args: {
     type: 'textfield',
+    children: '<any children />',
   },
 } as Meta<typeof Fieldset>;
 
 type Story = StoryObj<typeof Fieldset>;
 
-const Template: StoryFn<typeof Fieldset> = args => (
-  <Fieldset {...args}>
-    <div>{args.children || '<any children />'}</div>
-  </Fieldset>
-);
-
 export const WithoutLabel: Story = {
-  render: Template,
-
   args: {
     label: '',
   },
 };
 
 export const WithToolTip: Story = {
-  render: Template,
-
   args: {
     label: 'With tooltip',
     tooltip: 'Hiya!',
   },
 };
 
-export const WithErrors: Story = Template.bind([]);
-WithErrors.parameters = {
-  formik: {
-    initialValues: {someField: ''},
-    initialErrors: {someField: 'Some error'},
+export const WithErrors: Story = {
+  parameters: {
+    formik: {
+      initialValues: {someField: ''},
+      initialErrors: {someField: 'Some error'},
+    },
   },
-};
-WithErrors.args = {
-  label: 'Errors must be displayed',
-  children: <input type="text" />,
-  field: 'someField',
-};
-WithErrors.argTypes = {
-  children: {table: {disable: true}},
-};
-WithErrors.play = async ({canvasElement}) => {
-  const canvas = within(canvasElement);
-  await expect(canvas.queryByText('Some error')).toBeInTheDocument();
+  args: {
+    label: 'Errors must be displayed',
+    children: <input type="text" />,
+    field: 'someField',
+  },
+  argTypes: {
+    children: {table: {disable: true}},
+  },
+
+  play: async ({canvasElement}) => {
+    const canvas = within(canvasElement);
+    await expect(canvas.queryByText('Some error')).toBeInTheDocument();
+  },
 };
