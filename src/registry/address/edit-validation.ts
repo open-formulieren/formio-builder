20e2069a723f3ee2c76ee3582b8b2ed81c224d69
Viicos <65306057+Viicos@users.noreply.github.com>
2023-12-01 15:01:25 +0100

    [#65] Fix validation

--- src/registry/address/edit-validation.ts
+++ src/registry/address/edit-validation.ts
@@ -2,13 +2,20 @@ import {IntlShape} from 'react-intl';
 import {z} from 'zod';
 
 import {buildCommonSchema} from '@/registry/validation';
+
 import {POSTCODE_REGEX} from '../postcode/constants';
 
 // Constraints taken from the BRK API (apart from postcode which comes from our postcode component)
 const addressSchema = z.object({
-  postcode: z.string().regex(new RegExp(POSTCODE_REGEX)),
-  houseNumber: z.string().regex(/^\d{1,5}$/),
-  houseLetter: z.string().regex(/^[a-zA-Z]$/),
+  postcode: z.string().regex(new RegExp(POSTCODE_REGEX)).optional(),
+  houseNumber: z
+    .string()
+    .regex(/^\d{1,5}$/)
+    .optional(),
+  houseLetter: z
+    .string()
+    .regex(/^[a-zA-Z]$/)
+    .optional(),
   houseNumberAddition: z
     .string()
     .regex(/^([a-z,A-Z,0-9]){1,4}$/)
