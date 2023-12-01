7c3c025b01e94552e4ce2600e44fd4a2fd48768b
Sergei Maertens <sergei@maykinmedia.nl>
2023-12-01 17:41:34 +0100

    :sparkles: [#74] Implement edit form for fieldset component

--- src/registry/fieldset/index.ts
+++ src/registry/fieldset/index.ts
@@ -1,10 +1,10 @@
-// import EditForm from './edit';
-// import validationSchema from './edit-validation';
+import EditForm from './edit';
+import validationSchema from './edit-validation';
 import Preview from './preview';
 
 export default {
-  // edit: EditForm,
-  // editSchema: validationSchema,
+  edit: EditForm,
+  editSchema: validationSchema,
   preview: Preview,
   defaultValue: undefined, // a fieldset component does not hold a value itself
 };
