626e9459085fc571775cd2945d31d9ddf4b51e28
Sergei Maertens <sergei@maykinmedia.nl>
2023-12-13 11:16:31 +0100

    :mute: Remove excessive logging again
    It doesn't help in debugging/resolving test flakiness.

--- .storybook/test-runner.ts
+++ .storybook/test-runner.ts
@@ -1,15 +1,5 @@
 import {TestRunnerConfig} from '@storybook/test-runner';
 
-const config: TestRunnerConfig = {
-  preRender: async (page, context) => {
-    const {id} = context;
-    console.log(`Testing story with id: ${id}`);
-  },
-
-  postRender: async (page, context) => {
-    const {id} = context;
-    console.log(`Done testing story with id: ${id}`);
-  },
-};
+const config: TestRunnerConfig = {};
 
 export default config;
