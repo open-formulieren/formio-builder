1373c24c461703d6cbcbeae2ca469d3ae4beb3b9
Sergei Maertens <sergei@maykinmedia.nl>
2023-12-15 18:29:33 +0100

    :green_heart: [#89] Fix jest tests
    The bundling/export is messed up, so importing @open-formulieren/leaflet-tools does not
    perform the expected side effects in a Jest context, causing cryptic crashes. It seems
    fine in Storybook and actually bundled code.
    
    For more information, see https://github.com/kartena/Proj4Leaflet/pull/147.
    
    If I was a beer drinker, this would be a good excuse to get properly intoxicated.

--- src/registry/map/index.ts
+++ src/registry/map/index.ts
@@ -1,3 +1,6 @@
+// a hack - this library has side effects because it patches L from leaflet.
+import 'proj4leaflet';
+
 import EditForm from './edit';
 import validationSchema from './edit-validation';
 import Preview from './preview';
