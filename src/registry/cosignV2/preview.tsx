0ab2f94385620321b7bfcde68358fe97d8fd28c3
SilviaAmAm <silvia@maykinmedia.nl>
2024-02-07 11:14:39 +0100

    :sparkles: [open-formulieren/open-forms#3257] Update builder cosign component

--- src/registry/cosignV2/preview.tsx
+++ src/registry/cosignV2/preview.tsx
@@ -1,6 +1,7 @@
 import {CosignV2ComponentSchema} from '@open-formulieren/types';
+import {FormattedMessage} from 'react-intl';
 
-import {TextField} from '@/components/formio';
+import {Description, FieldSet, TextField} from '@/components/formio';
 
 import {ComponentPreviewProps} from '../types';
 
@@ -12,19 +13,38 @@ import {ComponentPreviewProps} from '../types';
  * @open-formulieren/formio-renderer instead for a more accurate preview.
  */
 const Preview: React.FC<ComponentPreviewProps<CosignV2ComponentSchema>> = ({component}) => {
-  const {key, label, description, tooltip, validate = {}, autocomplete} = component;
+  const {key, label, validate = {}, description, checkBsn} = component;
   const {required = false} = validate;
   return (
-    <TextField
-      name={key}
-      multiple={false}
-      label={label}
-      description={description}
-      tooltip={tooltip}
-      required={required}
-      autoComplete={autocomplete}
-      type="email"
-    />
+    <FieldSet field={key} label={label}>
+      {description && <Description text={description} />}
+      <TextField
+        name={`${key}.email`}
+        multiple={false}
+        label={
+          <FormattedMessage
+            description="Label for the email of the cosigner"
+            defaultMessage="Cosigner's email address"
+          />
+        }
+        required={required}
+        type="email"
+      />
+      {checkBsn && (
+        <TextField
+          name={`${key}.bsn`}
+          multiple={false}
+          label={
+            <FormattedMessage
+              description="Label for BSN of cosigner"
+              defaultMessage="Cosigner's BSN"
+            />
+          }
+          required={required}
+          inputMask={'999999999'}
+        />
+      )}
+    </FieldSet>
   );
 };
 
