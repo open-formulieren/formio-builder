62c43c755e84f72f18ba5b29bc68789577c798f2
Sergei Maertens <sergei@maykinmedia.nl>
2024-03-20 14:44:34 +0100

    :children_crossing: [#483] Manage option description translations

--- src/components/builder/values/i18n.tsx
+++ src/components/builder/values/i18n.tsx
@@ -3,7 +3,7 @@ import {useFormikContext} from 'formik';
 import {useContext} from 'react';
 import {FormattedMessage, useIntl} from 'react-intl';
 
-import {TextField} from '@/components/formio';
+import {TextArea, TextField} from '@/components/formio';
 import {PathsForValueType} from '@/types';
 
 import {ComponentTranslationsContext} from '../i18n';
@@ -65,30 +65,38 @@ export function ValuesTranslations<S>({name}: ValuesTranslationsProps<S>) {
           </>
         ))}
       </tbody>
+
       <tbody>
         <tr>
           <th colSpan={3} style={{textAlign: 'end'}}>
             <FormattedMessage
-              description="Values/options translations table header"
-              defaultMessage="Choice/option descriptions translations"
+              description="Values/options description translations table header"
+              defaultMessage="Description translations"
             />
           </th>
         </tr>
-        {options.map(({description}, index) => (
+        {options.map(({description, value, label}, index) => (
           <tr key={`option-description-${index}`}>
             <td>
-              <span id={`option-${index}-description`}>{description}</span>
+              <span id={`option-${index}-label`}>
+                <FormattedMessage
+                  description="Label for option description location"
+                  defaultMessage="Option description (<option></option>)"
+                  values={{
+                    option: () => <code>{value || '-'}</code>,
+                  }}
+                />
+              </span>
             </td>
-            <td colSpan={2}>
-              <TextField
+
+            <td>
+              <div aria-describedby={`option-${index}-label`}>{description || '-'}</div>
+            </td>
+
+            <td>
+              <TextArea
                 name={`${name}[${index}]openForms.translations.${activeLanguage}.description`}
-                aria-label={intl.formatMessage(
-                  {
-                    description: 'Accessible label for option description translation field',
-                    defaultMessage: 'Translation for option with value "{value}"',
-                  },
-                  {value: description}
-                )}
+                aria-labelledby={`option-${index}-label`}
               />
             </td>
           </tr>
