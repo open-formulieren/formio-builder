d19d6d6647d19c7ffe681852c93b1a3e984bfe44
Sergei Maertens <sergei@maykinmedia.nl>
2024-07-23 09:44:44 +0200

    wip

--- src/TranslationsManager.tsx
+++ src/TranslationsManager.tsx
@@ -1,7 +1,35 @@
 // TODO: move into its own library!
+import {useReducer} from 'react';
+
+interface Message {
+  originalDefault: string; // default from code
+  defaultMessage: string; // translation
+  description: string;
+  isTranslated?: boolean;
+}
+
+type MessageCatalog = Record<string, Message>;
+
+const reducer = ({type, action}): MessageCatalog => {
+  return {};
+};
 
 const TranslationsManager: React.FC = () => {
-  return <>hello</>;
+  const [state, dispatch] = useReducer(reducer, {});
+
+  return (
+    <div>
+      <h1>Translations</h1>
+
+      <div>
+        <textarea
+          name="sourceFile"
+          placeholder="Paste the translations JSON file contents here"
+          style={{inlineSize: '100%'}}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default TranslationsManager;
