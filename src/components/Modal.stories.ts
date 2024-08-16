3e90bddc28ad2d845eeca81a9f4473cb9ae08902
Sergei Maertens <sergei@maykinmedia.nl>
2024-08-16 14:20:58 +0200

    :sparkles: Implement a modal component with bootstrap styles
    Formio's modal component is based on or inspired by bootstrap. To make
    transitioning easier, we apply the same formio classnames so that we
    can share the styling.
    
    The modal implementation needs some tweaking to match the markup used
    in Formio/bootstrap, but other than that it's pretty straight forward.
    
    The ModalContext primarily serves to mute warnings and make interaction
    testing easier in Storybook, by:
    
    * disabling the aria-hidden integration on the app element
    * injecting the modal node in the right place in the DOM so that it's
      within the canvas

---
+++ src/components/Modal.stories.ts
@@ -0,0 +1,22 @@
+import {Meta, StoryObj} from '@storybook/react';
+import {fn} from '@storybook/test';
+
+import Modal from './Modal';
+
+export default {
+  title: 'Generic/Modal',
+  component: Modal,
+  parameters: {
+    modal: {noModal: true},
+  },
+  args: {
+    isOpen: true,
+    closeModal: fn(),
+    className: 'additional-classnames',
+    children: 'Modal content',
+  },
+} satisfies Meta<typeof Modal>;
+
+type Story = StoryObj<typeof Modal>;
+
+export const Default: Story = {};
