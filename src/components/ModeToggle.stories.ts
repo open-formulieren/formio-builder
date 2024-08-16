ab96924f5af56f8d29a000504e0316025bd29041
Sergei Maertens <sergei@maykinmedia.nl>
2024-08-16 14:25:28 +0200

    :sparkles: Implement a mode toggle component
    Currently there are 'display' mode toggles in the component preview
    side panel and the columns preview (desktop vs mobile). The full form
    preview will also have a toggle mode between 'structure' and 'webform',
    so instead of copying over the same implementation, this is extracted
    as its own re-usable component.
    
    Added the implementation, type integration and Storybook story + docs.

---
+++ src/components/ModeToggle.stories.ts
@@ -0,0 +1,25 @@
+import {Meta, StoryObj} from '@storybook/react';
+import {fn} from '@storybook/test';
+
+import ModeToggle from './ModeToggle';
+
+export default {
+  title: 'Generic/ModeToggle',
+  component: ModeToggle,
+  parameters: {
+    modal: {noModal: true},
+  },
+  args: {
+    name: 'story',
+    modes: [
+      {value: 'mode1', label: 'Mode 1'},
+      {value: 'mode2', label: 'Mode 2'},
+    ],
+    currentMode: 'mode2',
+    onToggle: fn(),
+  },
+} satisfies Meta<typeof ModeToggle>;
+
+type Story = StoryObj<typeof ModeToggle>;
+
+export const Default: Story = {};
