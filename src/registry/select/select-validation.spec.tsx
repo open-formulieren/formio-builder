119bcffbf1772c46e7edf06d2740ff7a073255c7
Sergei Maertens <sergei@maykinmedia.nl>
2024-08-16 17:32:37 +0200

    :white_check_mark: Replace interaction test with jest test
    The focus/blur and race conditions in CI lead to flaky tests.

--- src/registry/select/select-validation.spec.tsx
+++ src/registry/select/select-validation.spec.tsx
@@ -55,3 +55,44 @@ test('Option values and labels are required fields', async () => {
   expect(await screen.findByText('The option label is a required field.')).toBeVisible();
   expect(await screen.findByText('The option value is a required field.')).toBeVisible();
 });
+
+test('There is always at least one option', async () => {
+  const onSubmit = jest.fn();
+  const component = {
+    id: 'wqimsadk',
+    type: 'select',
+    key: 'select',
+    label: 'A select field',
+    dataSrc: 'values',
+    openForms: {
+      dataSrc: 'manual',
+      translations: {},
+    },
+    data: {
+      values: [],
+    },
+    defaultValue: '',
+  } satisfies SelectComponentSchema;
+
+  const builderInfo = {
+    title: 'Select',
+    icon: 'th-list',
+    group: 'basic',
+    weight: 70,
+    schema: {},
+  };
+  contextRender(
+    <ComponentEditForm
+      isNew
+      component={component}
+      builderInfo={builderInfo}
+      onCancel={jest.fn()}
+      onRemove={jest.fn()}
+      onSubmit={onSubmit}
+    />
+  );
+
+  // wait for first value to be added
+  const firstValueLabel = await screen.findByTestId('input-data.values[0].label');
+  expect(firstValueLabel).toBeVisible();
+});
