eda3299e734eb35587301a158b60551bb6da37b3
robinvandermolen <robin@maykinmedia.nl>
2025-09-25 16:42:20 +0200

    :ok_hand: [open-formulieren/open-forms#5191] Review comments

--- src/components/error/ErrorBoundary.stories.tsx
+++ src/components/error/ErrorBoundary.stories.tsx
@@ -1,26 +1,19 @@
-import {Meta, StoryObj} from '@storybook/react';
+import type {Meta, StoryObj} from '@storybook/react';
 
 import ErrorBoundary from './ErrorBoundary';
 
-const ThrowError = ({error}: {error: Error}) => {
+const ThrowError: React.FC<{error: Error}> = ({error}) => {
   throw error;
-};
-
-const render = () => {
-  const error = new Error('some error');
-  return (
-    <ErrorBoundary>
-      <ThrowError error={error} />
-    </ErrorBoundary>
-  );
 };
 
 export default {
   title: 'Generic/Error boundary',
-  render,
   component: ErrorBoundary,
   parameters: {
     modal: {noModal: true},
+  },
+  args: {
+    children: <ThrowError error={new Error('some error')} />,
   },
 } as Meta<typeof ErrorBoundary>;
 
