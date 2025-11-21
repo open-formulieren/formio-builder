de6f3d57e9c7f5874b019d8c7f0053ddf0c35a4b
hakkiai <abhishektripathikuberji@gmail.com>
2025-11-21 18:10:58 +0530

    refactor(phone-number): align validation structure with other components

--- src/registry/phonenumber/edit-validation.ts
+++ src/registry/phonenumber/edit-validation.ts
@@ -1,9 +1,9 @@
-import {defineMessage} from 'react-intl';
-import {z} from 'zod';
+import { defineMessage, type IntlShape } from 'react-intl';
+import { z } from 'zod';
 
-import {buildCommonSchema} from '@/registry/validation';
+import { buildCommonSchema } from '@/registry/validation';
 
-import {EditSchema} from '../types';
+import { EditSchema } from '../types';
 
 const PHONE_NUMBER_INVALID_MESSAGE = defineMessage({
   description: 'Validation error for phone number format.',
@@ -11,14 +11,28 @@ const PHONE_NUMBER_INVALID_MESSAGE = defineMessage({
     'Invalid phone number - a phone number may only contain digits, the + or - sign or spaces',
 });
 
-const schema: EditSchema = ({intl}) => {
-  const singleValueSchema = z.string().regex(/^[+0-9][- 0-9]+$/, {
-    message: intl.formatMessage(PHONE_NUMBER_INVALID_MESSAGE),
-  });
+const buildDefaultValueSchema = (intl: IntlShape) => {
+  const phoneNumberSchema = z
+    .string()
+    .regex(/^[+0-9][- 0-9]+$/, {
+      message: intl.formatMessage(PHONE_NUMBER_INVALID_MESSAGE),
+    })
+    .optional();
 
-  const defaultValue = z.union([singleValueSchema, z.array(singleValueSchema)]).optional();
+  // case for when component.multiple = false
+  const singleValueSchema = z
+    .object({ multiple: z.literal(false) })
+    .and(z.object({ defaultValue: phoneNumberSchema }));
 
-  return buildCommonSchema(intl).extend({defaultValue});
+  // case for when component.multiple = true
+  const multipleValueSchema = z
+    .object({ multiple: z.literal(true) })
+    .and(z.object({ defaultValue: phoneNumberSchema.array() }));
+
+  return singleValueSchema.or(multipleValueSchema);
 };
+
+const schema: EditSchema = ({ intl }) =>
+  buildCommonSchema(intl).and(buildDefaultValueSchema(intl));
 
 export default schema;
