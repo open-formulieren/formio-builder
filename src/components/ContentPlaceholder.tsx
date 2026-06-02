837bda4cc96114a049d334254e4031897fae0a0c
robinvandermolen <robin@maykinmedia.nl>
2026-06-02 16:39:28 +0200

    :truck: Moved ContentPlaceholder component

--- src/components/ContentPlaceholder.tsx
+++ src/components/ContentPlaceholder.tsx
@@ -1,16 +1,16 @@
 import clsx from 'clsx';
 import type {PropsWithChildren} from 'react';
 
-import './contentPlaceholder.scss';
+import './ContentPlaceholder.scss';
 
 interface ContentPlaceholderProps extends PropsWithChildren {
   variant: 'builder' | 'designer';
 }
 
-const Index: React.FC<ContentPlaceholderProps> = ({variant, children}) => (
+const ContentPlaceholder: React.FC<ContentPlaceholderProps> = ({variant, children}) => (
   <div className={clsx('offb-content-placeholder', `offb-content-placeholder--${variant}`)}>
     {children}
   </div>
 );
 
-export default Index;
+export default ContentPlaceholder;
