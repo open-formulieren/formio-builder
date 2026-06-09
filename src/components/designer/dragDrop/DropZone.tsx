cdc57086a41db2d79594ad740042ba297af63be3
robinvandermolen <robin@maykinmedia.nl>
2026-06-09 16:17:01 +0200

    :sparkles: [#269] collisionPriority for dropzones and sortable components
    By setting different collisionPriority's we can specify which target should be considered first.
    
    Each dropzone updates the `collisionPriority` for itself and its children. This ensures that elements are correctly dragged into dropzones and that deeper dropzones get a higher priority than shallow dropzones.
    
    This also was intended as a solution for the issue with `getTargetIndex`, where the focus would switch between the dropzone and sortable items in that dropzone, causing the index to shift massively, but this doesn't seem to help as much as I would like.

--- src/components/designer/dragDrop/DropZone.tsx
+++ src/components/designer/dragDrop/DropZone.tsx
@@ -1,6 +1,7 @@
 import {useDroppable} from '@dnd-kit/react';
 
 import './DropZone.scss';
+import {DropzoneContext, useDropzoneContext} from './context';
 
 export interface DropZoneProps {
   id: string;
@@ -11,11 +12,14 @@ export interface DropZoneProps {
  * A drag-and-drop container for components.
  */
 const DropZone: React.FC<DropZoneProps> = ({id, children}) => {
-  const {ref} = useDroppable({id});
+  const {collisionPriority} = useDropzoneContext();
+  const {ref} = useDroppable({id, collisionPriority});
 
   return (
     <div className="offb-drop-zone" data-testid={id} ref={ref}>
-      {children}
+      <DropzoneContext.Provider value={{collisionPriority: collisionPriority + 1}}>
+        {children}
+      </DropzoneContext.Provider>
     </div>
   );
 };
