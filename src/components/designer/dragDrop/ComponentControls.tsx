443697fa9e7df3d8ac29acf17ae2913e9f2b0131
robinvandermolen <robin@maykinmedia.nl>
2026-09-01 15:08:36 +0200

    :recycle: [#334] Re-structuring component controls

--- src/components/designer/dragDrop/ComponentControls.tsx
+++ src/components/designer/dragDrop/ComponentControls.tsx
@@ -1,6 +1,8 @@
 import type {AnyComponentSchema} from '@open-formulieren/types';
+import {clsx} from 'clsx';
 import {useContext} from 'react';
-import {FormattedMessage, defineMessage, useIntl} from 'react-intl';
+import type {MessageDescriptor} from 'react-intl';
+import {FormattedMessage, defineMessages, useIntl} from 'react-intl';
 
 import {DesignerContext} from '@/context';
 
@@ -10,48 +12,84 @@ interface ComponentControlsProps {
   component: AnyComponentSchema;
 }
 
-const ComponentControls: React.FC<ComponentControlsProps> = ({component}) => {
-  const intl = useIntl();
-  const {editComponent, deleteComponent} = useContext(DesignerContext);
-
-  const editComponentTitle = defineMessage({
+const COMPONENT_CONTROL_LABELS = defineMessages({
+  editComponent: {
     description: 'Form designer preview edit component button title',
     defaultMessage: 'Edit component',
-  });
-  const deleteComponentTitle = defineMessage({
+  },
+  deleteComponent: {
     description: 'Form designer preview delete component button title',
     defaultMessage: 'Delete component',
-  });
+  },
+});
+
+const ComponentControls: React.FC<ComponentControlsProps> = ({component}) => {
+  const intl = useIntl();
 
   return (
-    <div className="offb-component-controls btn-group">
-      <button
-        className="btn btn-xxs btn-secondary"
-        title={intl.formatMessage(editComponentTitle)}
-        onClick={event => {
-          event.preventDefault();
-          editComponent(component);
-        }}
-      >
-        <span className="sr-only">
-          <FormattedMessage {...editComponentTitle} />
-        </span>
-        <i className="fa fa-cog" aria-hidden="true" />
-      </button>
-      <button
-        className="btn btn-xxs btn-danger"
-        title={intl.formatMessage(deleteComponentTitle)}
-        onClick={event => {
-          event.preventDefault();
-          deleteComponent(component);
-        }}
-      >
-        <span className="sr-only">
-          <FormattedMessage {...deleteComponentTitle} />
-        </span>
-        <i className="fa fa-trash-can" aria-hidden="true" />
-      </button>
+    <div
+      className="offb-component-controls btn-group"
+      role="group"
+      aria-label={intl.formatMessage({
+        description: 'Form designer preview component controls accessible label',
+        defaultMessage: 'Component controls',
+      })}
+    >
+      <EditControl component={component} />
+      <DeleteControl component={component} />
     </div>
+  );
+};
+
+const EditControl: React.FC<ComponentControlsProps> = ({component}) => {
+  const {editComponent} = useContext(DesignerContext);
+
+  return (
+    <ComponentControl
+      className="btn-secondary"
+      icon="cog"
+      label={COMPONENT_CONTROL_LABELS.editComponent}
+      onClick={() => editComponent(component)}
+    />
+  );
+};
+
+const DeleteControl: React.FC<ComponentControlsProps> = ({component}) => {
+  const {deleteComponent} = useContext(DesignerContext);
+
+  return (
+    <ComponentControl
+      className="btn-danger"
+      icon="trash-can"
+      label={COMPONENT_CONTROL_LABELS.deleteComponent}
+      onClick={() => deleteComponent(component)}
+    />
+  );
+};
+
+interface ComponentControlProps {
+  className: string;
+  icon: string;
+  label: MessageDescriptor;
+  onClick: () => void;
+}
+
+const ComponentControl: React.FC<ComponentControlProps> = ({className, icon, label, onClick}) => {
+  const intl = useIntl();
+  return (
+    <button
+      className={clsx('btn btn-xxs', className)}
+      title={intl.formatMessage(label)}
+      onClick={event => {
+        event.preventDefault();
+        onClick();
+      }}
+    >
+      <span className="sr-only">
+        <FormattedMessage {...label} />
+      </span>
+      <i className={`fa fa-${icon}`} aria-hidden="true" />
+    </button>
   );
 };
 
